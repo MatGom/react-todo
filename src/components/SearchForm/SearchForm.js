ba77f53aeeb38853cdaab24566e9d7aa38cf1b51
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-01-21 20:57:44 +0000

    Update TextInput

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,7 @@ import TextInput from '../TextInput/TextInput';
 const SearchForm = () => {
   return (
     <form className={styles.searchForm}>
-      <TextInput />
+      <TextInput placeholder='Search…' />
       <button className={styles.button}>Search</button>
     </form>
   );
