f46249f2ce8f3842bbc140f605895778d6032de5
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-06 21:26:51 +0100

    Add lists

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,12 +9,16 @@ export const getFilteredCards = ({ cards, searchString }, columnId) =>
 
 export const getAllColumns = state => state.columns;
 
+export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
+
+export const getColumnsByList = ({ columns }, listId) => columns.filter(column => column.listId === listId);
+
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
-export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload })
+export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
 const reducer = (state, action) => {
   switch (action.type) {
