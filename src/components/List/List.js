f035924ee509917aa3df1ccae7d1280b3e2fef9f
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-06 23:04:01 +0100

    Add Navigate to List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,12 +5,14 @@ import ColumnForm from './../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 import { getColumnsByList, getListById } from '../../redux/store';
 import { useParams } from 'react-router';
+import { Navigate } from 'react-router-dom';
 
 const List = () => {
   const { listId } = useParams();
   const columns = useSelector(state => getColumnsByList(state, listId));
   const listData = useSelector(state => getListById(state, listId));
 
+  if(!listData) return <Navigate to="/" />
   return (
     <div className={styles.list}>
       <header className={styles.header}>
